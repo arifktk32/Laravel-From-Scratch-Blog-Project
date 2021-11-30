08575966a544fccd37ee8c0b3dd9d5689b77c932
Arif Iqbal <arifktk32@gmail.com>
2021-11-30 20:09:48 +0500

    Ep#11: Make the posts index page dynamic by getting file contents using the Laravel File Facades

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
 class Post {
     public static function find($slug) {
@@ -13,5 +14,10 @@ class Post {
         }
 
         return cache()->remember('posts'.$slug, 1200, fn() => file_get_contents($path));
+    }
+    
+    public static function all() {
+        $posts = File::files( resource_path("posts/") );
+        return array_map(fn($post) => $post->getContents(), $posts);
     }
 }
