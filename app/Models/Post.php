036ab630c1bb47f035c81ea6301e1635a6eba6df
Arif Iqbal <arifktk32@gmail.com>
2021-12-14 21:44:49 +0500

    Ep#23 Route Model Binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     protected $guarded = []; // Allow Mass Assignment for all field. !! Watch Out !!
     // protected $guarded = ["id"]; // Guard id column and allow the rest
     // protected $fillable = ["title", "slug", "excerpt", "body"];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
