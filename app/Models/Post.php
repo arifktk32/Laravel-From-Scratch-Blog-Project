a86ffa9738ccec88f6393a9ca839fd0c076a10be
Arif Iqbal <arifktk32@gmail.com>
2021-12-15 22:05:09 +0500

    Ep#24 Your First Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,4 +17,8 @@ class Post extends Model
     {
         return 'slug';
     }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,7 @@
                 {{ $post->title }}
             </a>    
         </h1>
+        <p><a href="#">{{ $post->category->title }}</a></p>
         <p>{{ $post->excerpt }}</p>
     </article>
     @endforeach
